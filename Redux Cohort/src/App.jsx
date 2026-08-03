28e2afd6551dc5478bcd5997675ba1abfa464678
Primus <niteshsalian8652085233@gmail.com>
2026-08-03 11:21:41 +0530

    Add counter increment/decrement buttons in Redux Cohort App

--- Redux Cohort/src/App.jsx
+++ Redux Cohort/src/App.jsx
@@ -1,20 +1,27 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { store } from './app/store'
+import { decrement, increment } from './Features/counter.Slice'
 
 const App = () => {
 
-    let {count} = useSelector((store)=> store.counter)
+  let { count } = useSelector((store) => store.counter)
 
+    let dispatch = useDispatch()
 
 
   return (
     <div>
-      
+
       <h1>
         This Is Reduxxx! {count}
-      </h1>
 
+      </h1>
+      <button
+      onClick={()=>dispatch(increment())}
+      >+</button>
+      <button
+      onClick={()=>dispatch(decrement())}>-</button>
     </div>
   )
 }
