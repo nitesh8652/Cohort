cdbd3aa737dec29d22841b80a8a14d5d7d321601
Primus <niteshsalian8652085233@gmail.com>
2026-08-25 17:46:08 +0530

    CRUD NOTE D1

---
+++ Notes/server.js
@@ -0,0 +1,9 @@
+const app = require('./src/app')
+
+let PORT = 12000
+
+
+
+app.listen(PORT,()=>{
+    console.log(`Port is running on ${PORT}`)
+})
--- TypeScriptWithReact/index.ts
+++ TypeScriptWithReact/index.ts
@@ -1,10 +1,30 @@
-let userObj = {
-    address:{
-        street:'DANIYA',
-        veg:"palak",
-        state:'Rajasthan',
-        country:"India"
-
+type userObj = {
+    name: string,
+    team: string,
+    transformers: {
+        Arce: string,
+        Bulkhead: string,
+        Bumblebee: string
     }
 }
+
+
+let userObj: userObj = {
+
+    name: "Transformers Prime",
+    team: "Autobots",
+    transformers: {
+        Arce: 'Bike',
+        Bulkhead: "Suv",
+        Bumblebee: "Car",
+
+    }
+
+}
+
+let sum = (a:number, b:number) => {
+    return a + b
+}
+
+sum (55 , 89)
 
